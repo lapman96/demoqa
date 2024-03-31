c579e03af921fa442579d18c3897e1e8688f3e16
Serhii Lapin <Serhii_Lapin@epam.com>
2024-03-31 23:56:33 +0200

    update parallel test case execution

--- src/test/java/elements/BaseElementsTest.java
+++ src/test/java/elements/BaseElementsTest.java
@@ -8,10 +8,9 @@ public class BaseElementsTest {
     public static void setUp(){
         Configuration.browser = System.getProperty("browser");
         Configuration.timeout = 10000;
-        Configuration.browserSize = "1980x1080";
         Configuration.screenshots = true;
         Configuration.fastSetValue = true;
         Configuration.headless = true;
-        Configuration.reopenBrowserOnFail = false;
+        Configuration.reopenBrowserOnFail = true;
     }
 }
